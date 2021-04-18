54164eee53ff586fd9fdf9bfa4ebe02b771de0bc
Elaryan1 <pabloredondo95@gmail.com>
2021-04-18 17:43:55 +0200

    Audio patch1

---
+++ src/Assets/Scripts/playerControls.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControls : MonoBehaviour
+{
+    public AK.Wwise.Event jumpSound;
+    bool isPlayerJumping = false;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("up") || Input.GetKeyDown("joystick button 0"))
+        {
+            if (isPlayerJumping == false)
+            {
+                jumpSound.Post(this.gameObject);
+            }
+            
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        isPlayerJumping = true;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isPlayerJumping = false;
+    }
+}
---
+++ src/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,56 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID JUMP = 3833651337U;
+        static const AkUniqueID LABSOUND = 794348319U;
+        static const AkUniqueID LABSOUND_STOP = 1559551914U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace MUSIC_STATE
+        {
+            static const AkUniqueID GROUP = 3826569560U;
+
+            namespace STATE
+            {
+                static const AkUniqueID LOOP = 691006007U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID SILENCE = 3041563226U;
+            } // namespace STATE
+        } // namespace MUSIC_STATE
+
+    } // namespace STATES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
